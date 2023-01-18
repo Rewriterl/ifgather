dde906654455c68592a21df58415263b0ee0161f
Ryouji <1354413904@qq.com>
2023-01-18 14:24:34 +0800

    Add default management account creation method.

--- test/dao_test.go
+++ test/dao_test.go
@@ -3,11 +3,13 @@ package test
 import (
 	"context"
 	"fmt"
+	"github.com/Rewriterl/ifgather/internal/cmd"
 	"github.com/Rewriterl/ifgather/internal/dao"
 	"github.com/Rewriterl/ifgather/utility/ipquery"
 	_ "github.com/gogf/gf/contrib/drivers/pgsql/v2"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/test/gtest"
+	"regexp"
 	"testing"
 )
 
@@ -49,5 +51,20 @@ func TestIpQuery(t *testing.T) {
 		location := ipquery.QueryLocation(info)
 		fmt.Println(location)
 		gtest.AssertEQ(err, nil)
+	})
+}
+
+func TestPasswd(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		upper := regexp.MustCompile(`[A-Z]`)
+		lower := regexp.MustCompile(`[a-z]`)
+		number := regexp.MustCompile(`[0-9]`)
+		special := regexp.MustCompile(`[!@#\$%\^&\*\(\)_\+\-=\{\}\[\]\\|;:'",.<>\/?]`)
+		password := cmd.GeneratePassword(6, true, true, true, true)
+		fmt.Println(password)
+		gtest.AssertEQ(upper.MatchString(password) &&
+			lower.MatchString(password) &&
+			number.MatchString(password) &&
+			special.MatchString(password), true)
 	})
 }
