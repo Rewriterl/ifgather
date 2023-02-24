e4851528516756e2da5cf96aece8467d3e63349e
Ryouji <1354413904@qq.com>
2023-02-24 12:42:12 +0800

    Use autofdmax to improve concurrency stability and maintain concurrency performance

--- ifgather-client/go.mod
+++ ifgather-client/go.mod
@@ -2,4 +2,7 @@ module github.com/Rewriterl/ifgather-client
 
 go 1.15
 
-require github.com/gogf/gf/v2 v2.1.4
+require (
+	github.com/gogf/gf/v2 v2.1.4
+	github.com/projectdiscovery/fdmax v0.0.4 // indirect
+)
--- ifgather-client/main.go
+++ ifgather-client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "github.com/Rewriterl/ifgather-client/utility/config"
 	_ "github.com/Rewriterl/ifgather-client/utility/logger"
+	_ "github.com/projectdiscovery/fdmax/autofdmax"
 )
 
 func main() {
